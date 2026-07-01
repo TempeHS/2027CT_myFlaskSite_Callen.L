3bad01de6e95d50b3184d1e6a49764ac23997b58
Callen :D <callen.lin1@education.nsw.gov.au>
2026-07-01 03:57:52 +0000

    feature: create 404 page with route

--- app.py
+++ app.py
@@ -13,5 +13,10 @@ def contact():
     return render_template("contact.html")
 
 
+@app.errorhandler(404)
+def page_not_found(error):
+    return render_template("404.html"), 404
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
