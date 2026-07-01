56fe016077ab981ff35023aa0d246e416d7850e1
Callen :D <callen.lin1@education.nsw.gov.au>
2026-07-01 04:14:20 +0000

    wip: pages for about attribution privacy sitemap and support

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
+# Primary Pages Route
 @app.route("/")
 def home():
     return render_template("index.html")
@@ -13,6 +14,33 @@ def contact():
     return render_template("contact.html")
 
 
+# Footer Pages Route
+@app.route("/about")
+def about():
+    return render_template("pages/about.html")
+
+
+@app.route("/privacy")
+def privacy():
+    return render_template("pages/privacy.html")
+
+
+@app.route("/support")
+def support():
+    return render_template("pages/support.html")
+
+
+@app.route("/sitemap")
+def sitemap_page():
+    return render_template("pages/sitemap.html")
+
+
+@app.route("/attribution")
+def attribution():
+    return render_template("pages/attribution.html")
+
+
+# 404 Page Route
 @app.errorhandler(404)
 def page_not_found(error):
     return render_template("404.html"), 404
