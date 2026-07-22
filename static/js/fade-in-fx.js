5ed6eec051fd7c316a89ab47c9f2e63a00a02ba8
Callen :D <callen.lin1@education.nsw.gov.au>
2026-07-22 16:09:50 +0000

    feat(index): complete homepage with 3 items

--- static/js/fade-in-fx.js
+++ static/js/fade-in-fx.js
@@ -21,7 +21,7 @@ document.addEventListener("DOMContentLoaded", function () {
 document.addEventListener("DOMContentLoaded", function () {
   const observerOptions = {
     root: null,
-    threshold: 0.7,
+    threshold: 0.6,
   };
 
   const observer = new IntersectionObserver((entries, observer) => {
@@ -34,6 +34,26 @@ document.addEventListener("DOMContentLoaded", function () {
   }, observerOptions);
 
   document.querySelectorAll(".fade-in-fx-higher").forEach((section) => {
+    observer.observe(section);
+  });
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+  const observerOptions = {
+    root: null,
+    threshold: 0.3,
+  };
+
+  const observer = new IntersectionObserver((entries, observer) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add("active");
+        observer.unobserve(entry.target);
+      }
+    });
+  }, observerOptions);
+
+  document.querySelectorAll(".fade-in-fx-even-higher").forEach((section) => {
     observer.observe(section);
   });
 });
