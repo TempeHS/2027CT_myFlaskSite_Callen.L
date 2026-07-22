109dfb12c21b8dc78a27b23df2e81fbb37ae915e
Callen :D <callen.lin1@education.nsw.gov.au>
2026-07-22 04:12:10 +0000

    higher fade in threshold in .fade-in-fx.higher

--- static/js/fade-in-fx.js
+++ static/js/fade-in-fx.js
@@ -17,3 +17,23 @@ document.addEventListener("DOMContentLoaded", function () {
     observer.observe(section);
   });
 });
+
+document.addEventListener("DOMContentLoaded", function () {
+  const observerOptions = {
+    root: null,
+    threshold: 0.7,
+  };
+
+  const observer = new IntersectionObserver((entries, observer) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add("active");
+        observer.unobserve(entry.target);
+      }
+    });
+  }, observerOptions);
+
+  document.querySelectorAll(".fade-in-fx-higher").forEach((section) => {
+    observer.observe(section);
+  });
+});
